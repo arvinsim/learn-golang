3b181016b65f8215a9562c063247f70a97541bb9
Arthur Vincent Simon <developarvin@gmail.com>
2022-02-13 21:40:34 +0800

    Rename some variables to fix a warning

--- methods.go
+++ methods.go
@@ -2,20 +2,20 @@ package main
 
 import "fmt"
 
-type rect struct {
+type rec struct {
 	width, height int
 }
 
-func (r *rect) area() int {
+func (r *rec) area() int {
 	return r.width * r.height
 }
 
-func (r rect) perim() int {
+func (r rec) perim() int {
 	return 2*r.width + 2*r.height
 }
 
 func main() {
-	r := rect{width: 10, height: 5}
+	r := rec{width: 10, height: 5}
 
 	fmt.Println("area: ", r.area())
 	fmt.Println("perim:", r.perim())
