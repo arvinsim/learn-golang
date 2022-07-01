55ba3ee41dfb928ede66ae38b60c4f16e0c5d5ab
Arthur Vincent Simon <developarvin@gmail.com>
2022-07-02 07:50:09 +0800

    Format main

--- main.go
+++ main.go
@@ -3,25 +3,11 @@ package main
 import (
 	"fmt"
 	"learn-golang/leetcode"
-	"learn-golang/practice"
 )
 
 func main() {
-	//outputResult("saippuakivikauppias")
-	//outputResult("foobar")
+	// Should be false
+	result := leetcode.IsIsomorphic("badc", "bada")
 
-	nums := []int{1, 7, 3, 6, 5, 6}
-	result := leetcode.PivotIndex(nums)
-
-	fmt.Printf("%d", result)
-}
-
-func outputResult(text string) {
-	isPalindrome := practice.CheckIfPalindrome(text)
-
-	if isPalindrome {
-		fmt.Printf("'%s' is a palindrome\n", text)
-	} else {
-		fmt.Printf("'%s' is a not palindrome\n", text)
-	}
+	fmt.Printf("%t", result)
 }
